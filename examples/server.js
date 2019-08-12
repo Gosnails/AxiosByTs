d24f0ead83c0451bec1002d09dec1dc9f4d7fb05
Songzhiguo <Fu_shide@163.com>
2019-08-12 20:24:30 +0800

    feat(base/app.ts server.js): 编写 demo

--- examples/server.js
+++ examples/server.js
@@ -35,6 +35,23 @@ router.get('/base/get', function (req, res) {
     res.json(req.query)
 })
 
+router.post('/base/post', function (req, res) {
+    res.json(req.body)
+})
+
+router.post('/base/buffer', function (req, res) {
+    let msg = []
+    req.on('data', (chunk) => {
+        if (chunk) {
+            msg.push(chunk)
+        }
+    })
+    req.on('end', () => {
+        let buf = Buffer.concat(msg)
+        res.json(buf.toJSON())
+    })
+})
+
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
