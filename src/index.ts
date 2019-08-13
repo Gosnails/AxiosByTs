dd57259a62914e84dcfe46436c4e2d8e64953b86
Songzhiguo <Fu_shide@163.com>
2019-08-13 14:33:31 +0800

    feat: 支持配置 `headers` 属性

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AxiosRequestConfig } from './types'
 import { bulidURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/header'
 import xhr from './xhr'
 
 function axios(config: AxiosRequestConfig): void {
@@ -11,11 +12,17 @@ function axios(config: AxiosRequestConfig): void {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
+  config.headers = transformHeaders(config)
   config.data = transformRequestData(config)
 }
 
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
