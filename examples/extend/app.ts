420475708d34ba292dc65a77899902aec596fade
Songzhiguo <Fu_shide@163.com>
2019-08-14 11:35:23 +0800

    feat: 接口添加泛型参数
    接口添加泛型参数+ demo 编写

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,16 +1,34 @@
 import axios from '../../src/index'
 
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
-  }
-})
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
 
-axios('/extend/post', {
-  method: 'post',
-  data: {
-    msg: 'hello'
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+
+async function test() {
+  // user 被推断出为
+  // {
+  //  code: number,
+  //  result: { name: string, age: number },
+  //  message: string
+  // }
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
   }
-})
+}
+
+test()
--- examples/server.js
+++ examples/server.js
@@ -77,6 +77,17 @@ router.post('/extend/post', function (req, res) {
     })
 })
 
+router.get('/extend/user', function (req, res) {
+    res.json({
+        code: 0,
+        message: 'ok',
+        result: {
+            name: 'jack',
+            age: 18
+        }
+    })
+})
+
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
